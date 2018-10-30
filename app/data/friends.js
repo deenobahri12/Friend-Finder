fad9eaf210fbec334169a84ea9ed1baf41ebf608
deeno bahri <deenobahri12@gmail.com>
2018-10-30 02:06:14 -0500

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Ben',
+		photo: 'https://img.freepik.com/free-psd/abstract-background-design_1297-87.jpg?size=338c&ext=jpg',
+		scores: [
+			'5',
+			'3',
+			'4',
+			'2',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Bolo',
+		photo: 'https://images.unsplash.com/photo-1530482817083-29ae4b92ff15?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=44f4aebbd1e1371d5bf7dc22016c5d29&w=1000&q=80',
+		scores: [
+			'4',
+			'5',
+			'5',
+			'4',
+			'1',
+			'4',
+			'2',
+			'3',
+			'3',
+			'2'
+		]
+	},
+	{
+		name: 'Omar',
+		photo: 'https://images.pexels.com/photos/11744/pexels-photo-11744.jpeg?auto=compress&cs=tinysrgb&h=350',
+		scores: [
+			'3',
+			'2',
+			'1',
+			'4',
+			'4',
+			'3',
+			'2',
+			'2',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Jason',
+		photo: 'https://images.pexels.com/photos/1054289/pexels-photo-1054289.jpeg?auto=compress&cs=tinysrgb&h=350',
+		scores: [
+			'4',
+			'4',
+			'5',
+			'3',
+			'4',
+			'4',
+			'4',
+			'4',
+			'2',
+			'3'
+		]
+	},
+	{
+		name: 'Tim',
+		photo: 'https://ds4bdrko1q549.cloudfront.net/assets/videoblocks/images/media-type/backgrounds/MOST-DL-2x.jpg',
+		scores: [
+			'5',
+			'3',
+			'4',
+			'1',
+			'1',
+			'4',
+			'2',
+			'1',
+			'4',
+			'2'
+		]
+	},
+	{
+		name: 'Paul',
+		photo: 'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+		scores: [
+			'4',
+			'5',
+			'4',
+			'2',
+			'2',
+			'4',
+			'3',
+			'1',
+			'3',
+			'1'
+		]
+	},
+	{
+		name: 'Smith',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFhrbotXqrmCW8A-QTFbBOVQQU9s-nJVssaQLTn55Y9jeArYGMkg',
+		scores: [
+			'4',
+			'5',
+			'4',
+			'1',
+			'2',
+			'4',
+			'3',
+			'2',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Chris',
+		photo: 'https://wallpaperbrowse.com/media/images/78b1965dbb8d754bbeb6f9a1addf0d4f--galaxy-background-background-pictures.jpg',
+		scores: [
+			'5',
+			'4',
+			'1',
+			'4',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Greg',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgF-f5JAZeBjq-NXteHpidSWNxWCbby7x1mPCZfOaBwYYgTkIJ6w',
+		scores: [
+			'5',
+			'1',
+			'4',
+			'5',
+			'4',
+			'1',
+			'2',
+			'3',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Wayne',
+		photo: 'http://www.designshock.com/wp-content/uploads/2015/02/prisma.jpg',
+		scores: [
+			'1',
+			'3',
+			'4',
+			'4',
+			'4',
+			'4',
+			'3',
+			'2',
+			'2',
+			'4'
+		]
+	}
+];
+
+module.exports = friendsArray;
